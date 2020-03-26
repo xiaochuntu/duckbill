182b5c325e9b43fa29a5463dbe8b252ab20574c5
tuchunxiao <281917185@qq.com>
2020-03-26 11:11:31 +0800

    test

---
+++ src/Hello.php
@@ -0,0 +1,18 @@
+<?php
+namespace Xiaochuntu\Duckbill;
+/**
+ * Created by PhpStorm.
+ * User: tcx
+ * Date: 20-3-26
+ * Time: 上午11:10
+ */
+class Hello
+{
+    // 声明属性
+    public $var = 'Hello World!';
+
+    // 声明方法
+    public function say() {
+        echo $this->var;
+    }
+}
